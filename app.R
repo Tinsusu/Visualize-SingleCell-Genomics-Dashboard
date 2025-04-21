8b9d1ad1b5ab9c7379e083a9937355eb623971b8
TinSutSur <114982114+Tinsusu@users.noreply.github.com>
2025-04-21 12:46:56 +0200

    Add UMAP and TSNE
    add UMAP, gene plot and TSNE

--- app.R
+++ app.R
@@ -4,11 +4,18 @@ ui <- dashboardPage(
   dashboardHeader(title = "scRNAseq Plot"),
   dashboardSidebar(
       sidebarMenu(id='tab',
+                ## add useShinyjs, this one for anything in UI can be disable or enable dynamicly
                 useShinyjs(),
+                ## The first name is for human, the second name is the name for internal operation
+                ## So the server is going to recognize home while human will see Kick thing off here!
+                ## the third component is icon, here we use dna icon
                 menuItem("Kick Things Off Here!", tabName = "home", icon = icon("dna")),
                 menuItem("scRNAseq Investigator", tabName = "input", icon = icon("dot-circle")),
+                ## This part will appear on the side bar when a certain condition is met.
+                ## We only need it when we click scRNAseq icon >>> input.tab is kinda in-built input
                 conditionalPanel(condition = "input.tab == 'input'",
                       div(
+                        ### fileInput is also a buil-in function "file" is id for system but user goingto see Please upload
                           fileInput("file", "Please Upload File", multiple=TRUE, accept=c('.rds')),
                           actionButton("reset", "Reset", icon = icon("undo"), 
                                        style = "color: #fff; background-color: #C69C6D; border-radius: 50%; width: 87.25%; padding: 15px; font-size: 18px; text-align: center;"),
@@ -19,16 +26,19 @@ ui <- dashboardPage(
     )
   ), 
   
+  ## large space that will eventually contain results for us
+  ## We'r gonna set two menu items, which depend on what item we click on
   dashboardBody(
     tabItems(
       tabItem(tabName = "input", # tabItem refers to tab in sidebar (not main panel)
+              #tabsetpanel for we can add some subtabs too.
               tabsetPanel(id = 'main_tabs',
                           tabPanel("Follow Me please",
                                    includeMarkdown("./markdown/instructions.md")
                           )
               )
       ),
-      tabItem(tabName = "home",
+      tabItem(tabName = "home", #when we select home from slide bar
               tags$h1(HTML("
               <div style='
                 font-family: \"Comic Sans MS\", cursive, sans-serif;
@@ -45,17 +55,21 @@ ui <- dashboardPage(
                 🦄 Welcome to the Single-cell RNAseq Seurat Analysis App 🌈
               </div>
             ")) ,
+              
       )
     )
   )         
 )
 
+### input refers to anything that we interact with the app, click run, uploadfile ....
+### output refers to anything display on our app
 server <- function(input, output, session) {
+     # we need to extend the size of large file because shiny has a limit size
      options(shiny.maxRequestSize = 300*1024^2) #shiny has a limit size, so do this to extend it.
-     
-     shinyjs::disable("run") #disable run button 
-     
-     # we can use observe an event when a certain file uploadded, want to enable run button
+     # By default run is clickable, we need to disable it until we upload a file.
+     shinyjs::disable("run")  #disable run button diable(id of button)
+     # we can use observe an event when a certain file uploadded, 
+     ## then want to enable run button
      observe({
           if(is.null(input$file) != TRUE){
             shinyjs::enable("run")
@@ -63,13 +77,187 @@ server <- function(input, output, session) {
             shinyjs::disable("run")
           }
      })
-     
+      
      observeEvent(input$reset,{
          shinyjs::reset("file")
+         ## when we remove the file run button should not be able to work
          shinyjs::diable("run")
      })
      
+     observeEvent(input$run, {
+       shinyjs::disable("run")
+       
+       # show loading widget using Rpack
+       show_modal_spinner(text = "Now we are preparing for plots")
+       # obj <- load_seurat_obj(input$file$datapath) # we need data path for that temp file
+       ## so obj will either be seurat object or error vector so we can do simple check
+       ## so if its a vector show popup
+       
+       obj <- load_seurat_obj(input$file$datapath)
+       if (is.vector(obj)){
+         showModal(modalDialog(
+           title = "Error with file",
+           HTML("<h5>There is an error with the your file.</h5><br>",
+                paste(unlist(obj), collapse = "<br><br>")) ### error msg from load seurat function
+         ))
+         shinyjs::enable("run")
+         
+       } else {
+         
+         ######################################################################
+         ##### create renderplot for UMAP ###########
+         output$umap <- renderPlot({
+           if (!is.null(input$metadata_col)) {
+             create_metadata_UMAP(obj, input$metadata_col)
+           }
+         })
+         
+         
+         ######################################################################
+         ######## Create Render plot for gene #########
+         output$featurePlot <- renderPlot({
+           if (!is.null(input$gene)) {
+             create_feature_plot(obj, input$gene)
+           }
+         })
+         
+         
+         ######### Downloaad plot ###################
+         output$downloadFeaturePlot <- downloadHandler(
+           filename = function(){
+             paste0(input$gene, '_feature_plot', '.png')
+           },
+           content = function(file){
+             plot <- create_feature_plot(obj, input$gene)
+             ggsave(filename=file, width = 10, height = 5, type = "cairo")
+           }
+         )
+         output$download_umap <- downloadHandler(
+           filename = function(){
+             paste0(input$metadata_col, '_UMAP', '.png')
+           },
+           content = function(file){
+             plot <- create_metadata_UMAP(obj, input$metadata_col)
+             ggsave(filename=file, width = 10, height = 5, type = "cairo")
+           }
+         )
+         
+         output$download_tsne <- downloadHandler(
+           filename = function(){
+             paste0(input$metadata_col, '_TSNE', '.png')
+           },
+           content = function(file){
+             plot <- create_metadata_TSNE(obj, input$metadata_col)
+             ggsave(filename=file, width = 10, height = 5, type = "cairo")
+           }
+         )
+         
+         
+         
+         ######################################################################
+         ##### create renderplot for TSNE ###########
+         output$tsne <- renderPlot({
+           if (!is.null(input$metadata_col)) {
+             create_metadata_TSNE(obj, input$metadata_col)
+           }
+         })
+         
+         
+         ####################################################################
+         
+         insertTab(
+           inputId = "main_tabs",
+           tabPanel(
+             "UMAP",
+             ####### this part will devide two part, chart and dropdown menu
+             fluidRow(
+               column(
+                 width = 8,
+                 plotOutput(outputId = 'umap'), ##buildin function plot umap
+                 downloadButton("download_umap", "Download UMAP Plot") ## id = download_umap, name
+               ),
+               column(
+                 width = 4,
+                 selectizeInput("metadata_col", 
+                                "Metadata Column", 
+                                colnames(obj@meta.data) 
+                 )
+               )
+             ),
+             style = "height: 90%; width: 95%; padding-top: 5%;"
+           ),
+           select = TRUE
+         )
+         
+         insertTab(
+           inputId = "main_tabs",
+           tabPanel(
+             "Gene Expression",
+             fluidRow(
+               column(
+                 width = 8,
+                 plotOutput(outputId = 'featurePlot'),
+                 downloadButton("downloadFeaturePlot", "Download Gene Plot")
+               ),
+               column(
+                 width = 4,
+                 selectizeInput("gene", 
+                                "Genes", 
+                                rownames(obj)
+                 )
+               )
+             ),
+             style = "height: 90%; width: 95%; padding-top: 5%;"
+           )
+         )
+         ### Stop the spinner once the tabs created
+         
+#         insertTab(
+#           inputId = "main_tabs",
+#           tabPanel("t-sne")
+#         )
+         
+         insertTab(
+           inputId = "main_tabs",
+           tabPanel(
+             "TSNE",
+             ####### this part will devide two part, chart and dropdown menu
+             fluidRow(
+               column(
+                 width = 8,
+                 plotOutput(outputId = 'tsne'), ##buildin function plot tsne
+                 downloadButton("download_tsne", "Download TSNE Plot") ## id = download_tsne, name
+               ),
+               column(
+                 width = 4,
+                 selectizeInput("metadata_col", 
+                                "Metadata Column", 
+                                colnames(obj@meta.data) 
+                 )
+               )
+             ),
+             style = "height: 90%; width: 95%; padding-top: 5%;"
+           ),
+           select = TRUE
+         )
+         
+         remove_modal_spinner() ### then eenable run button
+         shinyjs::enable("run")
+         
+       }
+     })
+     
+     # Clear all sidebar inputs when 'Reset' button is clicked
+     observeEvent(input$reset, {
+       shinyjs::reset("file")
+       removeTab("main_tabs", "UMAP")
+       removeTab("main_tabs", "Gene Expression Plot")
+       shinyjs::disable("run")
+     
+     })
+     
 }
+
 
 shinyApp(ui, server)
 
--- global.R
+++ global.R
@@ -4,15 +4,28 @@ library(shiny)
 library(shinydashboard)
 library(shinyjs)
 library(tools)
+library(Seurat)
+library(shinybusy)
+library(dplyr)
+library(DT)
+library(shinydashboardPlus)
+library(ggplot2)
+library(shinybusy)
+library(glue)
+library(markdown)
+library(ggthemes)
+library(grid)
+library(png)
 
 #when we upload the file it will be kept in temp folder
-
+## 1. check if it is rds extenstion
+## 2. check if it is Seurat objcet
 # Read in file and perform validation.
 load_seurat_obj <- function(path){
   errors <- c()
   # check file extension
   if (!tolower(tools::file_ext(path)) == "rds") { # ignores case
-    errors <- c(errors, "Invalid rds file.")
+    errors <- c(errors, "Invalid file we neeed rds.") #add eror in error vector
     return(errors)
   }
   
@@ -27,15 +40,88 @@ load_seurat_obj <- function(path){
     }
   )
   
-  # Validate obj is a seurat object
+  # Validate obj is a seurat object inherit function is built-in in R
   if (!inherits(obj, "Seurat")){
-    errors <- c(errors, "File is not a seurat object")
+    errors <- c(errors, "The file is not a seurat object")
     return(errors)
   }
   
   return(obj)
 }
 
+
+create_metadata_UMAP <- function(obj, col){
+  if (col %in% c("nCount_RNA", "nFeature_RNA", "percent.mt")){
+    feature_value <- obj@meta.data[, col]
+    
+    # Extract UMAP coordinates and rename columns to UMAP_1 and UMAP_2
+    umap_coords <- as.data.frame(obj@reductions$umap@cell.embeddings)
+    colnames(umap_coords)[1:2] <- c("UMAP_1", "UMAP_2")
+    
+    # Combine with feature value
+    col_df <- data.frame(umap_coords, feature_value = feature_value)
+    
+    # Plot
+    umap <- ggplot(data = col_df) +
+      geom_point(mapping = aes(UMAP_1, UMAP_2, color = log10(feature_value)), size = 0.01) +
+      scale_colour_gradientn(colours = rainbow(7))
+  } else if (col %in% colnames(obj@meta.data)) {
+    umap <- DimPlot(obj, pt.size = .1, label = F, label.size = 4, group.by = col, reduction = "umap")
+  } else {
+    umap <- ggplot() +
+      theme_void() +
+      geom_text(aes(x = 0.5, y = 0.5, label = "collumn doesn't exist"), size = 20, color = "gray73", fontface = "bold") +
+      theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
+  }
+  return(umap)
+}
+
+
+create_feature_plot <- function(obj, gene) {
+  if (gene %in% rownames(obj)) {
+    FP <- FeaturePlot(obj, features = gene, pt.size = 0.001, combine = FALSE)
+  } else {
+    FP <- ggplot() + 
+      theme_void() + 
+      geom_text(aes(x = 0.5, y = 0.5, label = "Gene doesn't exist"), size = 20, color = "gray73", fontface = "bold") +
+      theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
+  }
+  return(FP)
+ }
+
  
+create_metadata_TSNE <- function(obj, col, dims = 1:10) {
+  # Automatically run t-SNE if missing
+  if (!("tsne" %in% names(obj@reductions))) {
+    message("t-SNE reduction not found. Running RunTSNE() with dims = ", paste(dims, collapse = ", "))
+    obj <- RunTSNE(obj, dims = dims)
+  }
+  
+  if (col %in% c("nCount_RNA", "nFeature_RNA", "percent.mt")) {
+    feature_value <- obj@meta.data[, col]
+    
+    # Extract t-SNE coordinates and rename columns
+    tsne_coords <- as.data.frame(obj@reductions$tsne@cell.embeddings)
+    colnames(tsne_coords)[1:2] <- c("TSNE_1", "TSNE_2")
+    
+    # Combine with metadata feature
+    col_df <- data.frame(tsne_coords, feature_value = feature_value)
+    
+    # Plot
+    tsne <- ggplot(data = col_df) +
+      geom_point(mapping = aes(TSNE_1, TSNE_2, color = log10(feature_value)), size = 0.01) +
+      scale_colour_gradientn(colours = rainbow(7))
+    
+  } else if (col %in% colnames(obj@meta.data)) {
+    tsne <- DimPlot(obj, pt.size = .1, label = FALSE, label.size = 4, group.by = col, reduction = "tsne")
+  } else {
+    tsne <- ggplot() +
+      theme_void() +
+      geom_text(aes(x = 0.5, y = 0.5, label = "col doesn't exist"), size = 20, color = "gray73", fontface = "bold") +
+      theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
+  }
+  
+  return(tsne)
+}
 
 
